347625a3c3183e5887865c1b1e450796a4de0b40
Mazur Sofia <mazursofia89@yandex.ru>
2026-03-06 14:27:53 +0500

    Update Header.h

--- Lab1_Task1/Lab1_Task1/Header.h
+++ Lab1_Task1/Lab1_Task1/Header.h
@@ -4,10 +4,15 @@
 #include <fstream>
 #include <vector>
 
+//FIX_ME: нет краткого описания функции
+// Функция выполняет сортировку вставками массива весов
 //FIX_ME: неправильное наименований функции и параметров
 //void insert_sort(std::vector<int>& w, std::vector<int>& p) {
 void InsertSort(std::vector<int>& weight, std::vector<int>& price);
 
+//FIX_ME: нет краткого описания функции
+// Решает задачу рюкзака методом динамического программирования
 //FIX_ME: неправильное наименований функции и параметров
 //int backpack(int W, std::vector<int>& wt, std::vector<int>& val, int n) {
 int CalculateBackpack(int backpackVolume, std::vector<int>& weight, std::vector<int>& values, int itemCount);
+
